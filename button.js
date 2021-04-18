7032e94ff3152f17436eeb70a950423b3cf95209
senalisa <82781389+senalisa@users.noreply.github.com>
2021-04-18 23:44:00 +0200

    Add files via upload

---
+++ button.js
@@ -0,0 +1,10 @@
+const scrollToTopBtn = document.querySelector("#scrollToTopBtn");
+
+scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo( {
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+
+    });
+});
---
+++ rooms.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+    <title> &copy; Theater 't Kapelletje</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="../CLE3/rooms.css">
+    <script src="https://kit.fontawesome.com/dcb2a70d47.js" crossorigin="anonymous"></script>
+</head>
+<header>
+    <div id= "logo"> <img src="https://theaterkapelletje.nl/wp-content/uploads/2018/08/cropped-logo-kapelletje-2018-def_307x70-1.png" width="170"  height="32" class="midden"> </div>
+    <nav>
+        <div id="emptyspace"></div>
+        <div id="navi"><a href="home.php">HOME</a></div>
+        <div id="navi"><a href="https://theaterkapelletje.nl/contact/">OVER ONS</a></div>
+        <div id="navi"><a href="https://theaterkapelletje.nl/familie/">STEUN ONS</a></div>
+    </nav>
+</header>
+<body>
+<button id="scrollToTopBtn" onclick="scrollToTopBtn">
+    <i class="fas fa-chevron-up"></i>
+</button>
+<div class="center">
+    <h2> Kies een kamer!</h2>
+    <h3>Podcast 1</h3>
+    <h4>23:59 - Podcast Lorem Ipsum</h4>
+    <p class="p1">Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum</p>
+    <div class="data-submit">
+        <input type="submit" class="btn" name="submit" value="Doe mee!"/>
+    </div>
+
+    <h3>Theater 4</h3>
+    <h4>23:59 - Theater Lorem Ipsum</h4>
+    <p class="p1">Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum</p>
+    <div class="data-submit">
+        <input type="submit" class="btn" name="submit" value="Doe mee!"/>
+    </div>
+
+    <h3>Beeldhouden 3</h3>
+    <h4>23:59 - Beeldhouden Lorem Ipsum</h4>
+    <p class="p1">Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum</p>
+    <div class="data-submit">
+        <input type="submit" class="btn" name="submit" value="Doe mee!"/>
+    </div>
+
+    <h3>Concert 1</h3>
+    <h4>23:59 - Concert Lorem Ipsum</h4>
+    <p class="p1">Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum</p>
+    <div class="data-submit">
+        <input type="submit" class="btn" name="submit" value="Doe mee!"/>
+    </div>
+
+    <h3>Cabaret 1</h3>
+    <h4>23:59 - Podcast Lorem Ipsum</h4>
+    <p class="p1">Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum <br> Lorem ipsum Lorem Ipsum Lorem Ipsum</p>
+    <div class="data-submit">
+        <input type="submit" class="btn" name="submit" value="Doe mee!"/>
+    </div>
+
+</div>
+</form>
+</div>
+<footer>
+    <p class="p2">© 2021 Theater 't Kapelletje
+        <br>
+        Van der Sluysstraat 176 - 3033 SR Rotterdam</p>
+</footer>
+<script src="button.js"></script>
+</body>
+</html>
